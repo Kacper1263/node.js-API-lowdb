19c8f0a246f758adf052660faf92ad861009d35e
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2019-08-24 22:12:16 +0200

    redirect from / to /api/v1/users

--- app.js
+++ app.js
@@ -76,13 +76,17 @@ tunnel.on('error', function(err){
 });
 //#endregion
 
+app.get('/', (req, res) =>{
+    res.redirect('/api/v1/users');
+});
+
 // get all users
 app.get('/api/v1/users', (req, res) => {
-  res.status(200).send({
-    success: 'true',
-    message: 'Users retrieved successfully. You can search by ID or name',
-    content: db.value()
-  })
+    res.status(200).send({
+        success: 'true',
+        message: 'Users retrieved successfully. You can search by ID or name',
+        content: db.value()
+    })
 });
 
 app.post('/api/v1/users', (req, res) => {
@@ -199,55 +203,55 @@ app.delete('/api/v1/users/:id', (req, res) => {
 });
 
 app.put('/api/v1/users/:id', (req, res) => {
-        const id = parseInt(req.params.id, 10);
-        
-        var oldID;
-        var oldName;
-        var oldDescription;
-        if (!req.body.name && !req.body.description) {
-            return res.status(400).send({
-                success: 'false',
-                message: 'No \'name\' or \'description\' given. Nothing will be changed',
+    const id = parseInt(req.params.id, 10);
+    
+    var oldID;
+    var oldName;
+    var oldDescription;
+    if (!req.body.name && !req.body.description) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'No \'name\' or \'description\' given. Nothing will be changed',
+        });
+    }
+
+
+    var list = db.get("users").value();
+    var success = false;
+    var indexToDelete = list.findIndex(user => user.id === id);
+
+    if(indexToDelete != -1){
+        oldID = list[indexToDelete].id;
+        oldName = list[indexToDelete].name;
+        oldDescription = list[indexToDelete].description;
+        success = true;
+
+        const newUser = {
+            id: oldID,
+            name: req.body.name || oldName,
+            description: req.body.description || oldDescription
+        }
+        if(db.get("users").splice(indexToDelete, 1, newUser).write()){ //Replace old with new
+            return res.status(201).send({
+                success: 'true',
+                message: 'User replaced successfully',
+                newUser,
             });
         }
-
-
-        var list = db.get("users").value();
-        var success = false;
-        var indexToDelete = list.findIndex(user => user.id === id);
-    
-        if(indexToDelete != -1){
-            oldID = list[indexToDelete].id;
-            oldName = list[indexToDelete].name;
-            oldDescription = list[indexToDelete].description;
-            success = true;
-
-            const newUser = {
-                id: oldID,
-                name: req.body.name || oldName,
-                description: req.body.description || oldDescription
-            }
-            if(db.get("users").splice(indexToDelete, 1, newUser).write()){ //Replace old with new
-                return res.status(201).send({
-                    success: 'true',
-                    message: 'User replaced successfully',
-                    newUser,
-                });
-            }
-            else{
-                return res.status(500).send({
-                    success: 'false',
-                    message: 'Old user found but can\'t be deleted',
-                });
-            }
-        }
-    
-        if(!success){
-            return res.status(404).send({
+        else{
+            return res.status(500).send({
                 success: 'false',
-                message: 'User not found. Wrong ID',
+                message: 'Old user found but can\'t be deleted',
             });
         }
+    }
+
+    if(!success){
+        return res.status(404).send({
+            success: 'false',
+            message: 'User not found. Wrong ID',
+        });
+    }        
 });
 
 app.listen(app.get('port'), () => {
